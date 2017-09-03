be017a53aba25999388e930b6946cbe239167426
Stefan-Lucian Bucur <slbucur@gmail.com>
2017-09-03 20:31:32 +0300

    Added base identicon building functions: hash_input and pick_color.

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,18 +1,43 @@
 defmodule Identicon do
   @moduledoc """
-  Documentation for Identicon.
+  Useful functions for creating an identicon based on a string
   """
 
+  def main(input) do
+    input
+    |> hash_input
+    |> pick_color
+  end
+
   @doc """
-  Hello world.
+  Returns an Images with a list of numbers based on the MD5 of a stric
 
   ## Examples
 
-      iex> Identicon.hello
-      :world
-
+      iex> Identicon.hash_input("banana")
+      %Identicon.Image{hex: [114, 179, 2, 191, 41, 122, 34, 138, 117, 115, 1, 35, 239, 239, 124, 65]}
   """
-  def hello do
-    :world
+  def hash_input(input) do
+    hex_image = :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
+    %Identicon.Image{hex: hex_image}
   end
+
+
+  @doc """
+  Picks the color based on an image generated through `hash_input`
+
+  ## Examples
+      iex> image = Identicon.hash_input("banana")
+      iex> Identicon.pick_color(image)
+      %Identicon.Image{
+        color: {114, 179, 2},
+        hex: [114, 179, 2, 191, 41, 122, 34, 138, 117, 115, 1, 35, 239, 239, 124, 65]}
+  """
+  def pick_color(image) do
+    %Identicon.Image{hex: [r, g, b | _remaining ]} = image
+    %Identicon.Image{image | color: {r, g, b}}
+  end
+
+
 end
