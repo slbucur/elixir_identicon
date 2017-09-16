1631e5c62a5a41505eb440a5ac44fac18cc17ef8
Stefan-Lucian Bucur <slbucur@gmail.com>
2017-09-16 05:07:05 +0300

    Working identicon builder.

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -9,6 +9,9 @@ defmodule Identicon do
     |> pick_color
     |> build_grid
     |> filter_odd_squares
+    |> build_pixel_map
+    |> draw_image
+    |> save_image(input)
   end
 
   @doc """
@@ -90,4 +93,33 @@ defmodule Identicon do
     %Identicon.Image{image| grid: grid}
   end
 
+
+  def build_pixel_map(%Identicon.Image{grid: grid} = image) do
+    pixel_map = Enum.map grid, fn({_code, index}) ->
+      x = rem(index, 5) * 50
+      y = div(index, 5) * 50
+
+      top_left = {x,  y}
+      bottom_right = {x + 50, y + 50}
+
+      {top_left, bottom_right}
+    end
+
+    %Identicon.Image{image| pixel_map: pixel_map}
+  end
+
+  def draw_image(%Identicon.Image{color: color, pixel_map: pixel_map}) do
+    image = :egd.create(250, 250)
+    fill = :egd.color(color)
+
+    Enum.each pixel_map, fn({start, stop}) ->
+      :egd.filledRectangle(image, start, stop, fill)
+    end
+
+    :egd.render(image)
+  end
+
+  def save_image(image, input) do
+    File.write("#{input}.png", image)
+  end
 end
